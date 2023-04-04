7baaf408cd3f83214f8e772fe72f65f987473a42
birdcountindia <skimmer@birdcount.in>
2023-04-04 22:33:30 +0530

    metric: proportions of districts in each concern category

--- scripts/10_pipeline.R
+++ scripts/10_pipeline.R
@@ -27,28 +27,68 @@ source("scripts/03_thresholds.R")
 ### summaries (metrics to monitor)
 
 # in each state, how many districts
-state_dists <- states_sf %>% 
+state_dists <- dists_sf %>% 
+  st_drop_geometry() %>% 
   dplyr::select(-AREA) %>% 
-  st_join(dists_sf %>% dplyr::select(-AREA, -STATE.NAME), join = st_covers) %>% 
-  st_drop_geometry()
+  left_join(states_sf %>% dplyr::select(-AREA)) %>% 
+  st_drop_geometry() %>% 
+  group_by(STATE.NAME) %>% 
+  dplyr::summarise(TOT.DIST = n_distinct(DISTRICT.NAME))
 
 ### find out why some districts disappear
 # maybe don't do st_covers but do largest join
 
 # in each state, how many of each category
 summ_state <- concern_class %>% 
-  group_by(STATE.NAME, CONCERN) %>% 
+  # removing districts of no concern (NA)
+  filter(!is.na(CONCERN.FINE)) %>% 
+  group_by(STATE.NAME, CONCERN.FINE) %>% 
   summarise(NO.DIST = n_distinct(DISTRICT.NAME)) %>% 
   ungroup() %>% 
+  # to calculate proportion
+  left_join(state_dists) %>% 
+  mutate(PROP.DIST = NO.DIST/TOT.DIST) %>% 
+  dplyr::select(-NO.DIST, -TOT.DIST) %>% 
   filter(!is.na(STATE.NAME)) %>% 
-  pivot_wider(names_from = "CONCERN", values_from = "NO.DIST") %>% 
-  set_colnames(c("STATE.NAME", "CONCERN.2", "CONCERN.3", "CONCERN.4", "CONCERN.5")) %>% 
-  mutate(across(2:5, ~ replace_na(.x, 0))) %>% 
-  arrange(desc(CONCERN.5), desc(CONCERN.4), desc(CONCERN.3), desc(CONCERN.2))
+  # filling zeroes
+  group_by(STATE.NAME) %>% 
+  complete(CONCERN.FINE = 1:5,
+           fill = list(PROP.DIST = 0)) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = "CONCERN.FINE", values_from = "PROP.DIST") %>% 
+  set_colnames(c("STATE.NAME", "CONCERN.1", "CONCERN.2", "CONCERN.3", "CONCERN.4", "CONCERN.5")) %>% 
+  mutate(across(2:6, ~ replace_na(.x, 0))) %>% 
+  arrange(desc(CONCERN.5), desc(CONCERN.4), desc(CONCERN.3), desc(CONCERN.2), desc(CONCERN.1))
+
+# in country, how many of each category
+summ_nat <- concern_class %>% 
+  # removing districts of no concern (NA)
+  filter(!is.na(CONCERN.FINE)) %>% 
+  group_by(CONCERN.FINE) %>% 
+  summarise(NO.DIST = n_distinct(DISTRICT.NAME)) %>% 
+  ungroup() %>% 
+  # to calculate proportion
+  mutate(TOT.DIST = n_distinct(dists_sf$DISTRICT.NAME),
+         PROP.DIST = NO.DIST/TOT.DIST) %>% 
+  dplyr::select(-NO.DIST, -TOT.DIST) %>% 
+  complete(CONCERN.FINE = 1:5,
+           fill = list(PROP.DIST = 0)) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = "CONCERN.FINE", values_from = "PROP.DIST") %>% 
+  set_colnames(c("CONCERN.1", "CONCERN.2", "CONCERN.3", "CONCERN.4", "CONCERN.5")) %>% 
+  mutate(across(everything(), ~ replace_na(.x, 0))) %>% 
+  arrange(desc(CONCERN.5), desc(CONCERN.4), desc(CONCERN.3), desc(CONCERN.2), desc(CONCERN.1))
+
+
+# writing summary objects
+write_xlsx(x = list("National level" = summ_nat,
+                    "State level" = summ_state),
+           path = "outputs/summary1.xlsx")
+
 
 
 # bins for number of lists per district
-n_bins <- temp_sum %>% 
+n_bins <- concern_class %>% 
   distinct(N.DIST) %>% 
   arrange(N.DIST) %>% 
   rownames_to_column("ROW") 
@@ -62,7 +102,7 @@ n_bins <- n_bins %>%
 
 
 
-plot1_base <- temp_sum %>% 
+plot1_base <- summ_state %>% 
   left_join(dists_sf %>% dplyr::select(-STATE.NAME, -AREA)) %>% 
   ggplot() +
   # india outline
@@ -109,35 +149,6 @@ plot1 <- ((plot1_base +
 ggsave(plot1, filename = "outputs/plot1.png", 
        dpi = 300, width = 24, height = 24, units = "in")  
 
-
-plot2_base <- temp_sum2 %>% 
-  left_join(states_sf) %>% 
-  ggplot() +
-  # india outline
-  geom_sf(data = india_sf, fill = "#D3D6D9") +
-  theme_classic() +
-  theme(axis.line = element_blank(),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank())
-
-plot2 <- (plot2_base +
-            geom_sf(aes(geometry = STATE.GEOM, fill = CONCERN.5)) +
-            scale_fill_viridis_c(option = "inferno", direction = -1,
-                                 name = "No. of concern 5\ndistricts")) |
-  (plot2_base +
-     geom_sf(aes(geometry = STATE.GEOM, fill = CONCERN.4)) +
-     scale_fill_viridis_c(option = "inferno", direction = -1,
-                          name = "No. of concern 4\ndistricts")) |
-  (plot2_base +
-     geom_sf(aes(geometry = STATE.GEOM, fill = CONCERN.3)) +
-     scale_fill_viridis_c(option = "inferno", direction = -1,
-                          name = "No. of concern 3\ndistricts"))
-
-ggsave(plot2, filename = "outputs/plot2.png", 
-       dpi = 300, width = 36, height = 14, units = "in")  
 
 
 
