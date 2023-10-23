c979eb1d2f15f1d9a2af167e0e26647c98f1f710
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2023-10-23 11:30:21 +0530

    updated paths for testing on my system

--- scripts/00_params.R
+++ scripts/00_params.R
@@ -18,8 +18,8 @@ require(glue)
 #   (today() - months(1)) %>%
 #     floor_date(unit = "month")
 # }
-cur_date <- "2023-05-01" %>% as_date()
-rel_date <- "2023-04-01" %>% as_date()
+cur_date <- "2023-03-01" %>% as_date()
+rel_date <- "2023-02-01" %>% as_date()
 
 
 cur_year <- cur_date %>% year()
@@ -31,10 +31,10 @@ rel_month_num <- rel_date %>% month()
 rel_month_lab <- rel_date %>% month(label = T, abbr = T) 
 
 
-maindatapath <-  glue("../ebird-datasets/EBD/ebd_IN_rel{rel_month_lab}-{rel_year}.RData")
-slicedatapath <-  glue("../ebird-datasets/EBD/ebd_IN_rel{rel_month_lab}-{rel_year}_slice.RData")
+# maindatapath <-  glue("../ebird-datasets/EBD/ebd_IN_rel{rel_month_lab}-{rel_year}.RData")
+# slicedatapath <-  glue("../ebird-datasets/EBD/ebd_IN_rel{rel_month_lab}-{rel_year}_slice.RData")
 # maindatapath <-  "../ebird-datasets/EBD/ebd_IN_relNov-2022.RData"
 # slicedatapath <-  "../ebird-datasets/EBD/ebd_IN_relNov-2022_slice.RData"
-# # test data
-# maindatapath <-  glue("ebd_IN_rel{rel_month_lab}-{rel_year}.RData")
-# slicedatapath <-  glue("ebd_IN_rel{rel_month_lab}-{rel_year}_slice.RData")
+# test data
+maindatapath <-  glue("ebd_IN_rel{rel_month_lab}-{rel_year}.RData")
+slicedatapath <-  glue("ebd_IN_rel{rel_month_lab}-{rel_year}_slice.RData")
